171cde9a9197c6a0e55f8e6b5dd2ac9d6287f1ad
i-yam-jeremy <i-yam-jeremy@users.noreply.github.com>
2018-11-10 19:11:26 -0800

    Added basic sprite
    Will probably update later but it works for now

--- src/index.js
+++ src/index.js
@@ -1,34 +1,41 @@
 import 'phaser';
 
-var config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: {
-        preload: preload,
-        create: create
-    }
+let config = {
+	type: Phaser.AUTO,
+	parent: 'preserve',
+	width: 800,
+	height: 600,
+	physics: {
+		default: 'matter',
+		matter: {
+			// debug: true
+		}
+	},
+	scene: {
+		preload: preload,
+		create: create,
+		update: update
+	}
 };
 
-var game = new Phaser.Game(config);
+let game = new Phaser.Game(config);
 
-function preload ()
-{
-    this.load.image('logo', 'assets/logo.png');
+function preload() {
+	this.load.image('jam-jar', 'assets/jam-jar.png');
+	this.load.json('jam-jar-shape', 'assets/jam-jar-shape.json');
 }
 
-function create ()
-{
-    var logo = this.add.image(400, 150, 'logo');
+function create() {
+	let jarShape = this.cache.json.get('jam-jar-shape');
+	this.matter.world.setBounds(0, 0, game.config.width, game.config.height);
+	//TODO this.add.image(0, 0, 'background').setOrigin(0, 0);
+	let jar = this.matter.add.sprite(0, 0, 'jam-jar', {shape: jarShape['jam-jar']});
+	jar.setPosition(400 + jar.centerOfMass.x, 300 + jar.centerOfMass.y);
+	jar.scaleX = jar.scaleY = 2.0;
+	let jar2 = this.matter.add.sprite(0, 0, 'jam-jar', {shape: jarShape['jam-jar']});
+	jar2.setPosition(410 + jar2.centerOfMass.x, 325 + jar2.centerOfMass.y);
+}
 
-    this.tweens.add({
-        targets: logo,
-        y: 450,
-        duration: 2000,
-        ease: 'Power2',
-        yoyo: true,
-        loop: -1
-    });
+function update() {
 
 }
